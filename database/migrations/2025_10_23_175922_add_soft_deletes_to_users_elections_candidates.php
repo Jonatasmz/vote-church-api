267e4020354bf57cc42cd5939112945225a2dc2d
Jônatas Zulske <jonatas@alamocomunicacao.com.br>
2025-10-23 15:03:30 -0300

    feat: implementando perfil visualizador

---
+++ database/migrations/2025_10_23_175922_add_soft_deletes_to_users_elections_candidates.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+
+        Schema::table('elections', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+
+        Schema::table('candidates', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+
+        Schema::table('elections', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+
+        Schema::table('candidates', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+    }
+};
